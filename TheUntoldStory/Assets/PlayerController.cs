d4cf5ba1d91251b58d9061de21f7fd56758317ee
BUEdevelopers <moutasemmohamed976@gmail.com>
2024-02-22 16:22:56 +0200

    Sherif with combat system, and monsoon animations

--- TheUntoldStory/Assets/PlayerController.cs
+++ TheUntoldStory/Assets/PlayerController.cs
@@ -2,26 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class PlayerController : MonoBehaviour
 {
-    private Animator anim;
+    public Animator anim;
     public float moveSpeed;
     public float jumpHeight;
-    public float FlyingHeight;
     public bool isFacingRight;
     public KeyCode Spacebar;
     public KeyCode L;
     public KeyCode R;
     public KeyCode F;
+    public KeyCode A;
     public Transform groundCheck;
     public float groundCheckRadius;
     public LayerMask whatIsGround;
-    public Transform airCheck;
-    public float airCheckRadius;
-    public LayerMask whatIsAir;
     public bool grounded;
-    public bool flying;
+
+    public bool isAttacking = false;
+   //// public bool canMove = true;
+    public static PlayerController instance;
+
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
     void flip()
     {
         transform.localScale = new Vector3(-(transform.localScale.x), transform.localScale.y, transform.localScale.z);
@@ -32,15 +35,15 @@ public class PlayerController : MonoBehaviour
         GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
     }
 
-    void Fly()
+
+    private void Awake()
     {
-        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, FlyingHeight);
+        instance = this;
     }
 
     void FixedUpdate()
     {
         grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
-        flying = Physics2D.OverlapCircle(airCheck.position, airCheckRadius, whatIsAir);
 
     }
     // Start is called before the first frame update
@@ -53,20 +56,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(Spacebar) && grounded)
+        Attack();
+        if (Input.GetKeyDown(Spacebar) && grounded )//&& canMove)
         {
             
             Jump();
         }
-        if (Input.GetKeyDown(F) && flying)
+        if (Input.GetKeyDown(F))
         {
 
-            Fly();
+           
         }
 
 
 
-        if (Input.GetKey(L))
+        if (Input.GetKey(L) )//&& canMove)
         {
             
             GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
@@ -78,7 +82,7 @@ public class PlayerController : MonoBehaviour
             }
         }
 
-        if (Input.GetKey(R))
+        if (Input.GetKey(R) )//&& canMove)
         {
             
             GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
@@ -89,9 +93,41 @@ public class PlayerController : MonoBehaviour
                 isFacingRight = true;
             }
         }
+
+        
+        //if (Input.GetKeyDown(A))
+        //{
+        //    anim.SetTrigger("Attack");
+
+        //    Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        //    foreach(Collider2D enemy in hitEnemies)
+        //    {
+        //        Debug.Log("We hit " + enemy.name);
+        //    }
+        //}
         anim.SetFloat("Speed", Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
         anim.SetBool("Grounded", grounded);
-        anim.SetBool("Flying", flying);
 
     }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+        {
+            return;
+        }
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+    void Attack()
+    {
+        if (Input.GetKeyDown(A)&& !isAttacking)
+        {
+            isAttacking = true;
+        }
+    }
+
+    //}
 }
